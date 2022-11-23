58ae119f2fb076b119fd9822c676745dacc1e3ac
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-11-23 20:25:56 -0300

    cros api liberada

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
@@ -13,6 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 
 app.use('/', indexRouter);
 
