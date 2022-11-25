0e99d4427b3cd5cf83504b2529f8d19574316316
wcocuzza <wallace.cocuzza@gmail.com>
2022-11-25 01:29:56 -0300

    Ajustado os verbos na rotas

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ const ClientesController = require("../controllers/clientesController")
 router.get('/', HomeController.index);
 
 router.get('/clientes', ClientesController.index);
-router.post('/clientes', ClientesController.create);
-router.get('/clientes/:id', ClientesController.show);
-router.delete('/clientes/:id', ClientesController.delete);
-router.put('/clientes/:id', ClientesController.update);
+router.post('/cliente', ClientesController.create);
+router.get('/cliente/:id', ClientesController.show);
+router.delete('/cliente/:id', ClientesController.delete);
+router.put('/cliente/:id', ClientesController.update);
 
 
 module.exports = router;
